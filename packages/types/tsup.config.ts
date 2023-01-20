fa6a8333bed3c44d1f5af25a2c12ec51a25036d5
Alec <aleccortega@gmail.com>
2023-01-20 16:36:42 -0500

    chore: correctly build types

--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from 'tsup';
+import { config } from '@commonalityco/config-tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  outDir: 'dist',
-  clean: true,
-  format: ['esm'],
-});
+export default defineConfig(config);
