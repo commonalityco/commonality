339884b918523f82efc1e31c5bc1bbc6b9d661a2
Alec <aleccortega@gmail.com>
2023-07-10 12:18:54 -0400

    update gitignore

--- packages/cli/feature-cli/lib/core/logger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.logger = void 0;
-exports.logger = {
-  log(...arguments_) {
-    console.log(...arguments_);
-  },
-};
