53d868fb709b6b157e202bcaec4ab82c7dbd6ddb
Alec <aleccortega@gmail.com>
2023-08-13 15:13:17 -0400

    preconstruct build

---
+++ packages/commonality/src/index.ts
@@ -0,0 +1,19 @@
+import { Command } from 'commander';
+import { publish } from './commands/publish';
+import { init } from './commands/init';
+import { validate } from './commands/validate';
+import { open } from './commands/open';
+
+const program = new Command();
+
+program
+  .name('commonality')
+  .description('Infinitely scale your front-end ecosystem')
+  .version('1.0.0');
+
+program.addCommand(init);
+program.addCommand(publish);
+program.addCommand(validate);
+program.addCommand(open);
+
+program.parse(process.argv);
---
+++ packages/ui-core/src/index.ts
@@ -0,0 +1,14 @@
+export {
+  Navigation,
+  NavigationLogo,
+  Divider,
+  NavigationButton,
+} from './navigation';
+export { ThemeButton, ThemeButtonLoading } from './theme-button';
+export { Logo } from './logo';
+export { GradientFade } from './gradient-fade';
+
+export { NpmLogo } from './images/npm-logo';
+export { PnpmLogo } from './images/pnpm-logo';
+export { YarnLogo } from './images/yarn-logo';
+export { Markdown } from './markdown';
