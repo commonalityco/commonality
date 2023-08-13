7d3cd5585645b44bdea68b7332fbad39b3f1fd75
Alec <aleccortega@gmail.com>
2023-08-13 17:36:31 -0400

    ESM

--- packages/commonality/src/index.ts
+++ packages/commonality/src/index.ts
@@ -3,6 +3,10 @@ import { publish } from './commands/publish';
 import { init } from './commands/init';
 import { validate } from './commands/validate';
 import { open } from './commands/open';
+import updateNotifier from 'update-notifier';
+import packageJson from '../package.json';
+
+updateNotifier({ pkg: packageJson }).notify();
 
 const program = new Command();
 
