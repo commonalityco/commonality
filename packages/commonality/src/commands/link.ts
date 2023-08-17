d789acf6f8b8bbdfb5a9149eada2c9050d04752f
Alec <aleccortega@gmail.com>
2023-08-16 23:48:33 -0400

    add changeset

--- packages/commonality/src/commands/link.ts
+++ packages/commonality/src/commands/link.ts
@@ -2,28 +2,39 @@ import { Command } from 'commander';
 import { getRootDirectory } from '@commonalityco/data-project';
 import path from 'node:path';
 import fs from 'fs-extra';
+import { validateProjectStructure } from '../utils/validate-project-structure.js';
 
 const command = new Command();
 
 export const linkAction = async (options: { project: string }) => {
-  const rootDirectory = await getRootDirectory();
-  const projectConfigPath = path.join(
-    rootDirectory,
-    '.commonality/config.json',
-  );
-
-  const hasConfigFile = await fs.exists(projectConfigPath);
-
-  if (hasConfigFile) {
-    const existingConfig = await fs.readJson(projectConfigPath);
-    await fs.writeJson(projectConfigPath, {
-      ...existingConfig,
-      projectId: options.project,
+  try {
+    await validateProjectStructure({
+      directory: process.cwd(),
+      command,
     });
-  } else {
-    await fs.outputJson(projectConfigPath, {
-      projectId: options.project,
-    });
+
+    const rootDirectory = await getRootDirectory();
+    const projectConfigPath = path.join(
+      rootDirectory,
+      '.commonality/config.json',
+    );
+
+    const hasConfigFile = await fs.exists(projectConfigPath);
+
+    if (hasConfigFile) {
+      const existingConfig = await fs.readJson(projectConfigPath);
+      await fs.writeJson(projectConfigPath, {
+        ...existingConfig,
+        projectId: options.project,
+      });
+    } else {
+      await fs.outputJson(projectConfigPath, {
+        projectId: options.project,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    command.error('Failed to link project');
   }
 };
 
