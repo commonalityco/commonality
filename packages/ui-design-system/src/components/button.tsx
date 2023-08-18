a7691a8038bb0a93e3ded052379445828f07c594
Alec <aleccortega@gmail.com>
2023-08-18 00:46:35 -0400

    update

--- packages/ui-design-system/src/components/button.tsx
+++ packages/ui-design-system/src/components/button.tsx
@@ -25,14 +25,14 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2 space-x-1',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
+        icon: 'h-9 w-9 shrink-0',
       },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
     },
-  }
+  },
 );
 
 export interface ButtonProperties
@@ -51,7 +51,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProperties>(
         {...properties}
       />
     );
-  }
+  },
 );
 Button.displayName = 'Button';
 
--- packages/ui-design-system/src/index.ts
+++ packages/ui-design-system/src/index.ts
@@ -13,6 +13,7 @@ export * from './components/scroll-area';
 export * from './components/select';
 export * from './components/separator';
 export * from './components/sheet';
+export * from './components/skeleton';
 export * from './components/snippet';
 export * from './components/kbd';
 export * from './components/switch';
