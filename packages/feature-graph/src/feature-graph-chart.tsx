e2bc94f74ac0165396fb0ff1f50c727ecb8c9244
Alec <aleccortega@gmail.com>
2023-08-23 15:55:35 -0400

    finish feature-graph-chart

--- packages/feature-graph/src/feature-graph-chart.tsx
+++ packages/feature-graph/src/feature-graph-chart.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { PackageManager } from '@commonalityco/utils-core/constants';
 import {
   Constraint,
@@ -5,8 +6,15 @@ import {
   Package,
   Violation,
 } from '@commonalityco/types';
-import FeatureGraphChartContent from './feature-graph-chart-content.js';
 import { Package as PackageIcon } from 'lucide-react';
+import { GraphChart } from '@commonalityco/ui-graph/graph-chart';
+import { getElementDefinitions } from '@commonalityco/utils-graph/get-element-definitions';
+import { useEffect, useRef } from 'react';
+import { GraphContext } from './graph-provider.js';
+import FeatureGraphToolbar from './feature-graph-toolbar.js';
+import { cn } from '@commonalityco/ui-design-system/cn';
+import debounce from 'lodash.debounce';
+import { useTheme } from 'next-themes';
 
 interface GraphProperties {
   packageManager?: PackageManager;
@@ -23,6 +31,62 @@ export function FeatureGraphChart({
   constraints,
   dependencies,
 }: GraphProperties) {
+  const containerReference = useRef<HTMLDivElement>(null);
+  const { resolvedTheme: theme } = useTheme();
+  const actor = GraphContext.useActorRef();
+  const isLoading = GraphContext.useSelector(
+    (state) =>
+      state.matches('updating') ||
+      state.matches('rendering') ||
+      state.matches('uninitialized'),
+  );
+  const isEmpty = GraphContext.useSelector((state) => {
+    return state.matches('success') && state.context.elements.length === 0;
+  });
+  const isHovering = GraphContext.useSelector(
+    (state) => state.context.hoveredRenderNode,
+  );
+  const renderGraph = GraphContext.useSelector(
+    (state) => state.context.renderGraph,
+  );
+
+  useEffect(() => {
+    const listener = debounce(() => {
+      renderGraph?.resize();
+      actor.send('FIT');
+    }, 50);
+
+    window.addEventListener('resize', listener);
+
+    return () => window.removeEventListener('resize', listener);
+  }, [renderGraph]);
+
+  useEffect(() => {
+    if (!violations || !packages) {
+      return;
+    }
+
+    if (containerReference.current && packages && dependencies) {
+      actor.send({
+        type: 'INITIALIZE',
+        containerId: containerReference.current.id,
+        elements: getElementDefinitions({ packages, dependencies }),
+        theme: theme ?? 'light',
+        violations,
+      });
+    }
+
+    return () => {
+      actor.send({ type: 'DESTROY' });
+    };
+  }, [violations, packages]);
+
+  useEffect(() => {
+    if (!theme) return;
+
+    actor.send({ type: 'SET_THEME', theme });
+  }, [theme, actor.send]);
+
   const isZero = !packages?.length;
 
   if (isZero) {
@@ -43,13 +107,29 @@ export function FeatureGraphChart({
   }
 
   return (
-    <FeatureGraphChartContent
-      packages={packages}
-      dependencies={dependencies}
-      violations={violations}
-      constraints={constraints}
-      packageManager={packageManager}
-    />
+    <>
+      <FeatureGraphToolbar
+        packageManager={packageManager}
+        constraints={constraints}
+        packages={packages}
+        violations={violations}
+        onPackageClick={(packageName) => {
+          actor.send({ type: 'NODE_SELECT', packageName });
+        }}
+      />
+      <GraphChart
+        ref={containerReference}
+        loading={isLoading}
+        isEmpty={isEmpty}
+        className={cn({
+          'cursor-pointer': isHovering,
+          'cursor-grab active:cursor-grabbing': !isHovering,
+        })}
+        onShowAllPackages={() => {
+          actor.send({ type: 'SHOW_ALL' });
+        }}
+      />
+    </>
   );
 }
 
