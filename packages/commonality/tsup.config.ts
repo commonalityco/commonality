f4b3cd4c222cb884e3ba52201bbd054e23398891
Alec <aleccortega@gmail.com>
2023-08-24 16:37:35 -0400

    build using esbuild

--- packages/commonality/tsup.config.ts
+++ packages/commonality/tsup.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  clean: true,
   format: 'esm',
   minify: true,
-  metafile: true,
   banner: {
     js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
   },
