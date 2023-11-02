63e190d59d0ff7b8f483c819da911798a5195aae
Alec <aleccortega@gmail.com>
2023-11-01 23:56:27 -0400

    feat: start to move towards internal package pattern

--- .commonality/config.ts
+++ .commonality/config.ts
@@ -1,27 +1,49 @@
 import { defineConfig, defineConformer } from 'commonality';
 import * as recommended from 'commonality-conform-recommended';
 
+// Only use message (maybe change to name) and ensure uniqueness
 const ensureScript = defineConformer<{ name: string; value: string }>(
   (options) => ({
     name: 'COMMONALITY/ENSURE_SCRIPT',
-    validate: async ({ json }) => {
-      return Boolean(
-        await json('package.json').get(`scripts[${options?.name}]`),
-      );
-    },
-    fix: async ({ json }) => {
-      return json('package.json').set(
+    validate: async ({ json }) =>
+      !!json('package.json').get(`scripts[${options?.name}]`),
+
+    fix: async ({ json }) =>
+      json('package.json').set(
         `scripts[${options?.name}]`,
         options?.value ?? '',
-      );
-    },
+      ),
     message: `Packages must include a "${options?.name}" script.`,
   }),
 );
 
+const ensureInternalPackage = defineConformer(() => ({
+  name: 'COMMONALITY/ENSURE_INTERNAL_PACKAGE',
+  message: 'Internal packages must have a specific package.json configuration.',
+  validate: async ({ json }) => {
+    // Maybe introduce .equals matcher which handles stripping whitespace and regex matching with .matches
+    const packageJson = json('package.json');
+    return (
+      (await packageJson.get('main')) === 'src/index.ts' &&
+      (await packageJson.get('types')) === 'src/index.ts' &&
+      (await packageJson.get('publishConfig.main')) === 'dist/index.js' &&
+      (await packageJson.get('publishConfig.types')) === 'dist/index.d.ts'
+    );
+  },
+  fix: async ({ json }) => {
+    const packageJson = json('package.json');
+
+    await packageJson.set('main', 'src/index.ts');
+    await packageJson.set('types', 'src/index.ts');
+    await packageJson.set('publishConfig.main', 'dist/index.js');
+    await packageJson.set('publishConfig.types', 'dist/index.d.ts');
+  },
+}));
+
 export default defineConfig({
   projectId: '123',
   conformers: {
+    internal: [ensureInternalPackage()],
     buildable: [
       ensureScript({
         name: 'type-check',
