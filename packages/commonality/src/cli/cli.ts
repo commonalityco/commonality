8304f462a3285c9830d9aedaf67d9794b840b9a6
Alec <aleccortega@gmail.com>
2023-11-29 02:06:56 -0500

    remove remote connection flags

--- packages/commonality/src/cli/cli.ts
+++ packages/commonality/src/cli/cli.ts
@@ -1,9 +1,7 @@
 import { Command } from 'commander';
-import { publish } from './commands/publish.js';
 import { init } from './commands/init.js';
 import { constrain } from './commands/constrain/constrain.js';
 import { studio } from './commands/studio.js';
-import { link } from './commands/link.js';
 import { conform } from './commands/conform.js';
 import packageJson from '../../package.json';
 
@@ -15,10 +13,8 @@ program
   .version(packageJson.version);
 
 program.addCommand(init);
-program.addCommand(publish);
 program.addCommand(constrain);
 program.addCommand(studio);
-program.addCommand(link);
 program.addCommand(conform);
 
 program.parse(process.argv);
