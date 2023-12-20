6509a74a73bd7c751da362aba3f42409e6004366
Alec <aleccortega@gmail.com>
2023-12-20 00:17:38 -0500

    update integration tests

--- packages/commonality/test/integration/studio.test.ts
+++ packages/commonality/test/integration/studio.test.ts
@@ -32,11 +32,129 @@ describe('studio', () => {
   });
 
   test(
-    'logs the URL to open Commonality Studio',
+    'logs the URL to open Commonality Studio in a pnpm monorepo',
     async () => {
       const fixturePath = path.resolve(
         path.dirname(fileURLToPath(import.meta.url)),
         '../../test/fixtures/kitchen-sink',
+      );
+      await fs.copy(fixturePath, temporaryPath);
+
+      cliProcess = execa(
+        binPath,
+        ['studio', '--debug', '--port', String(preferredPort), '--install'],
+        {
+          cwd: temporaryPath,
+          stdout: 'pipe',
+        },
+      );
+
+      let output = '';
+      const stdoutMock = new Writable({
+        write(chunk, encoding, callback) {
+          output += stripAnsi(chunk.toString());
+          callback();
+        },
+      });
+
+      cliProcess.stdout?.pipe(stdoutMock);
+      cliProcess.stderr?.pipe(stdoutMock);
+
+      await vi.waitFor(() => {
+        expect(output).toContain('📦 Starting Commonality Studio...');
+      });
+
+      await vi.waitFor(() => {
+        expect(output).toContain(
+          `MISSING DEPENDENCY  Cannot find dependency '@commonalityco/studio'`,
+        );
+      });
+
+      await vi.waitFor(
+        () => {
+          expect(output).toContain(
+            `Viewable at: http://127.0.0.1:${preferredPort} (press ctrl-c to quit)`,
+          );
+        },
+        { timeout: 20_000 },
+      );
+
+      cliProcess.kill('SIGTERM', {
+        forceKillAfterTimeout: 2000,
+      });
+
+      await vi.waitFor(() => {
+        expect(output).toContain(`Successfully exited Commonality Studio`);
+      });
+    },
+    { timeout: 50_000 },
+  );
+
+  test(
+    'logs the URL to open Commonality Studio in an npm monorepo',
+    async () => {
+      const fixturePath = path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        '../../test/fixtures/kitchen-sink-npm',
+      );
+      await fs.copy(fixturePath, temporaryPath);
+
+      cliProcess = execa(
+        binPath,
+        ['studio', '--debug', '--port', String(preferredPort), '--install'],
+        {
+          cwd: temporaryPath,
+          stdout: 'pipe',
+        },
+      );
+
+      let output = '';
+      const stdoutMock = new Writable({
+        write(chunk, encoding, callback) {
+          output += stripAnsi(chunk.toString());
+          callback();
+        },
+      });
+
+      cliProcess.stdout?.pipe(stdoutMock);
+      cliProcess.stderr?.pipe(stdoutMock);
+
+      await vi.waitFor(() => {
+        expect(output).toContain('📦 Starting Commonality Studio...');
+      });
+
+      await vi.waitFor(() => {
+        expect(output).toContain(
+          `MISSING DEPENDENCY  Cannot find dependency '@commonalityco/studio'`,
+        );
+      });
+
+      await vi.waitFor(
+        () => {
+          expect(output).toContain(
+            `Viewable at: http://127.0.0.1:${preferredPort} (press ctrl-c to quit)`,
+          );
+        },
+        { timeout: 20_000 },
+      );
+
+      cliProcess.kill('SIGTERM', {
+        forceKillAfterTimeout: 2000,
+      });
+
+      await vi.waitFor(() => {
+        expect(output).toContain(`Successfully exited Commonality Studio`);
+      });
+    },
+    { timeout: 50_000 },
+  );
+
+  test(
+    'logs the URL to open Commonality Studio in a yarn monorepo',
+    async () => {
+      const fixturePath = path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        '../../test/fixtures/kitchen-sink-yarn',
       );
       await fs.copy(fixturePath, temporaryPath);
 
