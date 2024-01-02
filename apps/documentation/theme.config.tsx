947ffd444d727b6a24985adc9a1c7c9b3551eb11
Alec <aleccortega@gmail.com>
2024-01-02 12:30:32 -0500

    fix title

--- apps/documentation/theme.config.tsx
+++ apps/documentation/theme.config.tsx
@@ -21,12 +21,16 @@ const config: DocsThemeConfig = {
   },
   banner: {
     key: '0-beta',
-    text: '🎉 Commonality is now in beta',
+    text: 'Commonality is now in beta 🎉',
   },
   useNextSeoProps() {
-    return {
-      titleTemplate: '%s – Commonality',
-    };
+    const { asPath } = useRouter();
+
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – SWR',
+      };
+    }
   },
   head: function useHead() {
     const { title } = useConfig();
@@ -44,11 +48,11 @@ const config: DocsThemeConfig = {
         <meta httpEquiv="Content-Language" content="en" />
         <meta
           name="description"
-          content="Commonality helps you scale JavaScript monorepos and ecosystems with the tools you already love."
+          content="Commonality helps you scale JavaScript monorepos and package ecosystems with the tools you already love."
         />
         <meta
           name="og:description"
-          content="Commonality helps you scale JavaScript monorepos and ecosystems with the tools you already love."
+          content="Commonality helps you scale JavaScript monorepos and package ecosystems with the tools you already love."
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={socialCard} />
