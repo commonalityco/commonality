aec0261c2e0485f03a2ab19b85f94bb735529393
Alec <aleccortega@gmail.com>
2024-01-04 21:38:57 -0500

    test: update smoke test

--- apps/commonality/src/cli/cli.ts
+++ apps/commonality/src/cli/cli.ts
@@ -12,7 +12,7 @@ program
 
 program
   .name('commonality')
-  .description('Build bigger with the tools you already love.')
+  .description('Build bigger with the tools you already love')
   .version(packageJson.version);
 
 program.addCommand(check);
