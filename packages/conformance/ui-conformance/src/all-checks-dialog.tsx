4379a71abf4dbd82b244d533472675cc39144819
Alec <aleccortega@gmail.com>
2024-01-09 13:41:24 -0500

    feat: package search in all checks modal

--- packages/conformance/ui-conformance/src/all-checks-dialog.tsx
+++ packages/conformance/ui-conformance/src/all-checks-dialog.tsx
@@ -7,10 +7,11 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  Input,
   ScrollArea,
 } from '@commonalityco/ui-design-system';
 import { ConformanceResult } from '@commonalityco/utils-conformance';
-import { ComponentProps } from 'react';
+import { ComponentProps, useMemo, useState } from 'react';
 import { ConformanceResults } from './conformance-results-list';
 import { GradientFade } from '@commonalityco/ui-core';
 
@@ -20,6 +21,20 @@ export function AllChecksDialog({
 }: ComponentProps<typeof Dialog> & {
   results: Omit<ConformanceResult, 'fix'>[];
 }) {
+  const [search, setSearch] = useState('');
+
+  const filteredResults = useMemo(() => {
+    if (search === '') {
+      return results;
+    }
+
+    return results.filter((result) => {
+      return result.package.name.includes(search);
+    });
+  }, [results, search]);
+
+  const hasFilteredResults = filteredResults.length > 0;
+
   return (
     <Dialog {...props}>
       <DialogTrigger asChild>
@@ -29,10 +44,27 @@ export function AllChecksDialog({
         <DialogHeader>
           <DialogTitle>All checks</DialogTitle>
         </DialogHeader>
-        <ScrollArea className="max-h-[400px] py-2 relative">
-          <ConformanceResults results={results} />
-          <GradientFade placement="bottom" />
-        </ScrollArea>
+        {results.length > 0 ? (
+          <div>
+            <Input
+              aria-label="Search packages"
+              className="mb-3"
+              placeholder="Search packages"
+              onChange={(event) => setSearch(event.target.value)}
+            />
+
+            <p className="text-xs text-muted-foreground leading-none">{`${filteredResults.length} checks`}</p>
+          </div>
+        ) : undefined}
+        {hasFilteredResults || (!hasFilteredResults && !search) ? (
+          <ScrollArea className="max-h-[400px] py-2 relative">
+            <ConformanceResults results={filteredResults} />
+            <GradientFade placement="bottom" />
+          </ScrollArea>
+        ) : undefined}
+        {!hasFilteredResults && search ? (
+          <p className="text-center py-16">No packages match your filters</p>
+        ) : undefined}
         <DialogFooter>
           <Button
             variant="outline"
