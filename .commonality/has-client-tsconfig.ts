2a651a85a54f7fd2608412375bb49a3fd6dc6af5
Alec <aleccortega@gmail.com>
2024-02-08 16:58:57 -0500

    fix: revert debugging changes

--- .commonality/has-client-tsconfig.ts
+++ .commonality/has-client-tsconfig.ts
@@ -1,16 +1,11 @@
-import { Check } from 'commonality';
 import { hasJsonFile } from 'commonality-checks-recommended';
 
-// export default hasJsonFile('tsconfig.json', {
-//   extends: '@commonalityco/config-tsconfig/react.json',
-//   include: ['src/**/*.ts', 'src/**/*.tsx'],
-//   compilerOptions: {
-//     outDir: './dist',
-//     rootDir: './src',
-//     typeRoots: ['./node_modules/@types'],
-//   },
-// });
-
-export default {
-  message: '',
-} satisfies Check;
+export default hasJsonFile('tsconfig.json', {
+  extends: '@commonalityco/config-tsconfig/react.json',
+  include: ['src/**/*.ts', 'src/**/*.tsx'],
+  compilerOptions: {
+    outDir: './dist',
+    rootDir: './src',
+    typeRoots: ['./node_modules/@types'],
+  },
+});
