fba9c04a52c7d5aa3aafe2731d20e60dccb594eb
Alec Chernicki <aleccortega@gmail.com>
2024-02-12 21:12:14 -0500

    chore: better release tagging for error reporting (#316)

--- apps/studio/sentry.server.config.ts
+++ apps/studio/sentry.server.config.ts
@@ -1,3 +1,5 @@
+import packageJson from './package.json';
+
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
@@ -9,6 +11,10 @@ Sentry.init({
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
+
+  environment: process.env.NODE_ENV,
+
+  release: `${packageJson.version}-${process.env.NODE_ENV}`,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
