b0a480919f61177a64b947872ff67b591c6366ad
Alec <aleccortega@gmail.com>
2024-02-12 21:07:31 -0500

    chore: hyphenate version

--- apps/studio/sentry.server.config.ts
+++ apps/studio/sentry.server.config.ts
@@ -14,7 +14,8 @@ Sentry.init({
 
   environment: process.env.NODE_ENV,
 
-  release: packageJson.version + process.env.NODE_ENV,
+  release: `${packageJson.version}-${process.env.NODE_ENV}`,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
