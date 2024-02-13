14f2d185578039d99760cb48d51e04b923b045d0
Alec <aleccortega@gmail.com>
2024-02-13 00:28:45 -0500

    feat: simplify version and fix pino

--- packages/utils-core/src/logger.ts
+++ packages/utils-core/src/logger.ts
@@ -1,10 +1,10 @@
 import pino from 'pino';
 
-export const logger = process.env.NODE_ENV === 'development' ? pino({
+export const logger = pino({
   transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
     },
   },
-}) : pino();
+});
